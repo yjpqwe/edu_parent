86d3568ed66546c21be5b2d00051071d81bb8e7f
yjp <592926935@qq.com>
2020-06-13 16:08:00 +0800

    6

---
+++ service/service_cms/src/main/java/com/yjp/educms/controller/BannerFrontController.java
@@ -0,0 +1,22 @@
+package com.yjp.educms.controller;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 首页banner表 前端控制器
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-06-13
+ */
+@RestController
+@RequestMapping("/educms/bannerfront")
+@CrossOrigin
+public class BannerFrontController {
+
+}
+
