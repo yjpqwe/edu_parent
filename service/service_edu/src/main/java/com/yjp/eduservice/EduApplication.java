5ebe62fa19298a97ab4521baa5312a8d694c14bb
yjp <592926935@qq.com>
2020-06-21 22:58:25 +0800

    11

--- service/service_edu/src/main/java/com/yjp/eduservice/EduApplication.java
+++ service/service_edu/src/main/java/com/yjp/eduservice/EduApplication.java
@@ -2,13 +2,12 @@ package com.yjp.eduservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@EnableDiscoveryClient //服务注册
-@EnableFeignClients  //服务调用
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @ComponentScan(basePackages = {"com.yjp"})
 public class EduApplication {
 
